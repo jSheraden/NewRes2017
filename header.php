3a9f66be8568179e8d8e32442323bd074e17b313
joey <josasheraden@gmail.com>
2017-11-05 02:51:07 -0600

    Added header.php file.

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div id="site-header"></div>
