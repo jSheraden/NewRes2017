4794d3dbe144df8b5c19ccee1f2c950099ba7d87
joey <josasheraden@gmail.com>
2017-11-05 02:49:38 -0600

    Added index.php file.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<div id="page-header">
+  <span class="site-title"><?php bloginfo('name'); ?></div>
+  <div class="separator"></div>
+  <span class="site-tagline"><?php bloginfo('description'): ?></div>
+</div>
+
+<div id="page-content">
+  <div class="posts-wrapper">
+    <?php if (have_posts()): ?>
+      <?php while (have_posts()): ?>
+      	the_post();
+        get_template_part('content', get_post_format());
+      <?php endwhile; ?>
+    <?php else: ?>
+      <?php get_template_part('content', '404'); ?>
+    <?php endif; ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
