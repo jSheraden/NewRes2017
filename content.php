6dd840d9e749297754021ea530f5a8031905436f
joey <josasheraden@gmail.com>
2017-11-05 02:51:44 -0600

    Added content.php file.

---
+++ content.php
@@ -0,0 +1,15 @@
+<div class="post">
+  <?php $author_archive_link = get_author_posts_url(get_the_author_meta('ID')); ?>
+  <h3><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
+  <h5>
+    <i>
+      Posted by
+      <a class="author-archive-link" href="<?php echo $author_archive_link; ?>">
+        the_author();
+      </a>
+      on
+      <?php the_time('F js, Y g:i a'); ?>
+    </i>
+  </h5>
+  <?php the_content(); ?>
+</div>
