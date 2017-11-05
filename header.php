2be553e5ca8287cad410db58a4a87071822cbf4d
joey <josasheraden@gmail.com>
2017-11-05 03:56:50 -0600

    Fixed error in header.php, style.css should import properly.

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
   <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
   <?php wp_head(); ?>
 </head>
