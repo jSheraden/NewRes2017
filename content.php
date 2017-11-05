e05cafd0998aff36f563d6501d55f975c578ad68
joey <josasheraden@gmail.com>
2017-11-05 03:29:49 -0600

    Fixed error in content.php file.

--- content.php
+++ content.php
@@ -1,5 +1,3 @@
-<?php get_header(); ?>
-
 <div class="post">
   <?php $author_archive_link = get_author_posts_url(get_the_author_meta('ID')); ?>
   <h3><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -15,5 +13,3 @@
   </h5>
   <?php the_content(); ?>
 </div>
-
-<?php get_footer(); ?>
