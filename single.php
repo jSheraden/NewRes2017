66057e61bed903119f69ac87ef30ed339e62b439
joey <josasheraden@gmail.com>
2017-11-05 21:57:48 -0600

    Fixed datetime error in single.php file.

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
         <?php the_author(); ?>
       </a>
       on
-      <?php the_time('F js, Y g:i a'); ?>
+      <?php the_time('F jS, Y g:i a'); ?>
     </i>
   </h5>
   <?php the_content(); ?>
